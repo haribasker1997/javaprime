025048be9baae240612586108d1406db0aae0f23
haribasker1997 <31512665+haribasker1997@users.noreply.github.com>
2017-09-17 10:44:04 +0530

    Add files via upload

---
+++ asc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+#include <conio.h> 
+
+int main()
+ 
+{
+
+int number=0;
+
+clrscr();
+
+while(number<=255)
+
+{
+
+printf("character = %c",number)
+;
+number++;
+
+}
+
+return(0);
+
+}
